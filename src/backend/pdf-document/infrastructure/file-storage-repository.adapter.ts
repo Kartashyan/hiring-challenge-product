ad587735f7f4a3693afa13f85c9d4764c7404f89
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-07-09 20:08:48 +0200

    feat: Update file storage repository adapter to actually persist files

---
+++ src/backend/pdf-document/infrastructure/file-storage-repository.adapter.ts
@@ -0,0 +1,34 @@
+// src/infrastructure/services/LocalFileStorage.ts
+
+import * as fs from 'fs';
+import * as path from 'path';
+import { promisify } from 'util';
+import { FileStorage } from '../application/file-storage.port';
+
+const writeFile = promisify(fs.writeFile);
+const readFile = promisify(fs.readFile);
+const unlink = promisify(fs.unlink);
+
+export class LocalFileStorage implements FileStorage {
+  private storagePath: string;
+
+  constructor(storagePath: string) {
+    this.storagePath = storagePath;
+  }
+
+  async save(file: Buffer, fileName: string): Promise<string> {
+    const filePath = path.join(this.storagePath, fileName);
+    await writeFile(filePath, file);
+    return filePath;
+  }
+
+  async delete(fileName: string): Promise<void> {
+    const filePath = path.join(this.storagePath, fileName);
+    await unlink(filePath);
+  }
+
+  async get(fileName: string): Promise<Buffer> {
+    const filePath = path.join(this.storagePath, fileName);
+    return await readFile(filePath);
+  }
+}
