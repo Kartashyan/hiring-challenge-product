3f822a0c196f06366af3dbb498e72104bac9906b
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-07-09 20:10:01 +0200

    feat: Add express middleware for JSON parsing and document router

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,6 +1,10 @@
 import express from 'express';
+import { documentRouter } from './pdf-document/infrastructure/document.controller';
 
 const app = express();
+
+app.use(express.json());
+app.use('/document', documentRouter);
 
 console.log('Listening on port 3000');
 app.listen(3000);
