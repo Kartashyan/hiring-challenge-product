5d8d1256fae232a32fa21ad23c2c702efe762817
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-07-09 20:21:25 +0200

    feat: Add navigation menu to the app layout, utilize createBrowserRouter from react-router

--- src/frontend/app.tsx
+++ src/frontend/app.tsx
@@ -1,14 +1,45 @@
+import { Layout, Menu } from 'antd';
 import 'antd/dist/reset.css'
+import { Content, Header } from 'antd/es/layout/layout';
 
 import { StrictMode } from "react";
 import reactDOM from "react-dom/client";
+import { createBrowserRouter, Link, Outlet, RouterProvider } from 'react-router-dom';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [],
+  },
+]);
 
 function App() {
-  return <h1>Hello world!</h1>
+  return (
+    <Layout>
+      <Header>
+        <Menu theme="dark" mode="horizontal">
+          <Menu.Item key="1">
+            <Link to="/">Processed PDFs</Link>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Link to="/pending">Pending PDFs</Link>
+          </Menu.Item>
+          <Menu.Item key="3">
+            <Link to="/upload">Upload PDF</Link>
+          </Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: '0 50px', marginTop: '64px' }}>
+        <Outlet />
+      </Content>
+    </Layout>
+  )
 }
 
 reactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
