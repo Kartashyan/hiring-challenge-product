e58c0a8fe09ff260277d37afcb4bfb960fb2b262
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-07-09 18:52:29 +0200

    feat: Create Metadata value object for document metadata

---
+++ src/backend/pdf-document/domain/metadata.value-object.ts
@@ -0,0 +1,27 @@
+import { ValueObject } from "src/backend/core/value-object";
+import { DocumentKind } from "./document-kind.value-object";
+
+
+export type MetadataProps = {
+  name: string;
+  author: string;
+  kind: DocumentKind;
+}
+
+export class Metadata extends ValueObject<MetadataProps> {
+  constructor(
+    private readonly name: string, // Will becaome a value object in the future whne we need a validation
+    private readonly author: string,
+    private readonly kind: DocumentKind
+  ) {
+    super({ name, author, kind });
+  }
+
+  toObject() {
+    return {
+      name: this.name,
+      author: this.author,
+      kind: this.kind.value,
+    }
+  }
+}
