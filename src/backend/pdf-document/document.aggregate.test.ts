2c5cc7cdcad83d5f543e1e141642526db88d7896
Yeghishe <yeghishe.kartashyan@gmail.com>
2024-07-09 18:15:42 +0200

    feat: Add Document aggregate for managing document properties and actions

---
+++ src/backend/pdf-document/document.aggregate.test.ts
@@ -0,0 +1,42 @@
+import { beforeEach, describe, expect, it } from "vitest";
+import { DocumentKind } from "./document-kind.value-object";
+import { Document, DocumentProps, DocumentStatus } from "./document.aggregate";
+import { Metadata } from "./metadata.value-object";
+
+describe("Document", () => {
+    let document: Document;
+    let props: DocumentProps;
+
+    beforeEach(() => {
+        props = {
+            metadata: null,
+            uploadedAt: new Date(),
+            updatedAt: new Date(),
+            filePath: null,
+        };
+        document = Document.create(props);
+    });
+
+    it("should create a new document with default status", () => {
+        expect(document.status).toBe(DocumentStatus.UPLOADING);
+    });
+
+    it("should update the metadata of the document", () => {
+        const metadata: Metadata = new Metadata("Document Name", "Author Name", new DocumentKind("regulation"));
+        document.updateMetadata(metadata);
+        console.log(document.getMetadata());
+        expect(document.getMetadata()).toEqual(metadata);
+    });
+
+    it("should update the status of the document", () => {
+        const newStatus = DocumentStatus.PROCESSED;
+        document.updateStatus(newStatus);
+        expect(document.status).toBe(newStatus);
+    });
+
+    it("should update the file path of the document", () => {
+        const filePath = "/path/to/document.pdf";
+        document.updateFilePath(filePath);
+        expect(document.getFilePath()).toBe(filePath);
+    });
+});
---
+++ src/backend/pdf-document/document.aggregate.ts
@@ -0,0 +1,78 @@
+import { AggregateRoot } from "src/backend/core/aggregate-root";
+import { UID } from "src/backend/core/id";
+import { Metadata } from "./metadata.value-object";
+
+export enum DocumentStatus {
+    UPLOADING = "uploading",
+    PENDING = "pending",
+    PROCESSING = "processing",
+    PROCESSED = "processed",
+}
+
+export interface DocumentProps {
+    id?: string;
+    metadata: Metadata | null;
+    uploadedAt: Date;
+    updatedAt: Date;
+    filePath: string | null;
+}
+
+export class Document extends AggregateRoot<DocumentProps> {
+    #status: DocumentStatus;
+    constructor(_props: DocumentProps, id?: UID) {
+        super(_props, id);
+        this.#status = DocumentStatus.UPLOADING;
+    }
+
+    public static create(props: DocumentProps, id?: UID): Document {
+        const document = new Document(props, id);
+        const isNew = !id;
+        if (isNew) {
+            document.addDomainEvent({
+                name: "document.created",
+                occuredAt: new Date(),
+                aggregate: document,
+            });
+        }
+        return document;
+    }
+
+    public updateMetadata(metadata: Metadata): void {
+        this.props.metadata = metadata;
+        this.addDomainEvent({
+            name: "document.metadata.updated",
+            occuredAt: new Date(),
+            aggregate: this,
+        });
+    }
+
+    get status(): DocumentStatus {
+        return this.#status;
+    }
+
+    public updateStatus(status: DocumentStatus): void {
+        this.#status = status;
+        this.addDomainEvent({
+            name: "document.status.updated",
+            occuredAt: new Date(),
+            aggregate: this,
+        });
+    }
+
+    public updateFilePath(filePath: string): void {
+        this.props.filePath = filePath;
+        this.addDomainEvent({
+            name: "document.filepath.updated",
+            occuredAt: new Date(),
+            aggregate: this,
+        });
+    }
+
+    public getFilePath(): string | null {
+        return this.props.filePath;
+    }
+
+    public getMetadata(): Metadata | null {
+        return this.props.metadata;
+    }
+}
